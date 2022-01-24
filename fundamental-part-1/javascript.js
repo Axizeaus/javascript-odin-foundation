8141bbf2f51e4d68ebe48352b3b3da093e869121
Axizeaus <axizeaus@gmail.com>
2022-01-25 01:09:32 +0630

    finished fundamental part 1 of js, odin foundation course

---
+++ fundamental-part-1/javascript.js
@@ -0,0 +1,39 @@
+// console.log("Hello from the javascript!")
+
+// let message = "This is coming from javascript file";
+// let what = "Name", profection = " learner ";
+// alert(message);
+// alert(what);
+
+// const PI = 22/7;
+// console.log(PI);
+
+
+// let Name = "John";
+// let admin = Name;
+// console.log(admin);
+
+// let this_planet = "earth";
+// let current_visitor = "default";
+// console.log(this_planet)
+// console.log(current_visitor)
+
+// const birthday = "01.01.2001";
+// const age = 21;
+// console.log(birthday)
+// console.log(age)
+
+console.log(23 + 96);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13
+let percantage = actual / max
+
