232837983d045d31666a4a7fe4898d3f8bf23e73
Axizeaus <axizeaus@gmail.com>
2022-01-31 08:15:00 +0630

    finished assignment reading, tested on browser console and some practice codes

---
+++ fundamental-part-3/practice.js
@@ -0,0 +1,16 @@
+function add7(num){
+    return num + 7;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function capitalize(text){
+    text = text.toLowerCase();
+    return text[0].toUpperCase() + text.slice(1);
+}
+
+function last_letter(text){
+    return text.slice(-1);
+}
